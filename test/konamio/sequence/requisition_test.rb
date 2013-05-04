dc1f09f04f688cec0ec89bc5a3418282cc8cfe5c
Tad Hosford <tad.hosford@gmail.com>
2013-05-04 15:19:05 -0500

    Adds optional block argument to K::S::R.execute!
     - Furthers tests

--- test/konamio/sequence/requisition_test.rb
+++ test/konamio/sequence/requisition_test.rb
@@ -4,12 +4,33 @@ describe Konamio::Sequence::Requisition do
   before do
     @subject = Konamio::Sequence::Requisition
     @options = {
-      prompt: "Foo diddly doobardy party hardy",
-      listener: @listener = MiniTest::Mock.new,
-      sequence: "a",
-      speaker:  @speaker  = MiniTest::Mock.new,
-      confirmation: "Good job, you."
+      input:    @input            = MiniTest::Mock.new,
+      output:   @output           = MiniTest::Mock.new,
+      prompt:   @prompt           = "Foo diddly doobardy party hardy",
+      listener: @listener         = MiniTest::Mock.new,
+      speaker:  @speaker          = MiniTest::Mock.new,
+      sequence: @sequence         = ["a"],
+      confirmation: @confirmation = "Good job, you."
     }
+
+    @speaker_instance = MiniTest::Mock.new
+    @speaker_result   = MiniTest::Mock.new
+    @speaker_instance.expect(:execute!, @speaker_result)
+
+    @listener_instance = MiniTest::Mock.new
+    @listener_result   = MiniTest::Mock.new
+    @listener_instance.expect(:execute!, @listener_result)
+
+    @speaker.expect(:new,  @speaker_instance,  [{ prompt: @prompt, output: @output }])
+    @listener.expect(:new, @listener_instance, [{ sequence: @sequence, input: @input }])
+    @data_hash = MiniTest::Mock.new
+    @listener_result.expect(:data, @data_hash)
+    @data_hash.expect(:[], "", [:sequence])
+
+    @speaker_confirmation_instance = MiniTest::Mock.new
+    @speaker_confirmation_result = MiniTest::Mock.new
+    @speaker.expect(:new, @speaker_confirmation_instance, [{ prompt: @confirmation, output: @output }])
+    @speaker_confirmation_instance.expect(:execute!, @speaker_confirmation_result)
   end
 
   it "can be initialized" do
@@ -17,5 +38,6 @@ describe Konamio::Sequence::Requisition do
   end
 
   it "returns a result object" do
+    @subject.new(@options).execute!
   end
 end
